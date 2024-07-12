80da6c4b18f3736359c7667d34087e43e7456651
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-07-12 16:34:50 +0300

    New solutions.

---
+++ src/U1_WorkingWithAbstraction/L1_Lab/P01_PointInRectangle/Rectangle.java
@@ -0,0 +1,15 @@
+package U1_WorkingWithAbstraction.L1_Lab.P01_PointInRectangle;
+
+public class Rectangle {
+    private Point A;
+    private Point C;
+
+    public Rectangle(Point A, Point C) {
+        this.A = A;
+        this.C = C;
+    }
+
+    public boolean contains(Point x) {
+        return x.greaterOrEqual(A) && x.lessOrEqual(C);
+    }
+}
